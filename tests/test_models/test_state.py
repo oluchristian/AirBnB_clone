80387457f93063b162f9315227a20c0c5b46151e
Eze Israel John <ezeisrael499@gmail.com>
2024-02-10 16:45:12 +0100

    Added the test_state file

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+""" The test_state module """
+
+from models.state import State
+from unittest import TestCase
+
+
+class TestState(TestCase):
+    """ The Test case for """
