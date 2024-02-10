c0a831e3e801a89671d8d0b08b1f007265b56076
Eze Israel John <ezeisrael499@gmail.com>
2024-02-10 22:52:47 +0100

    Added some changes to this file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+""" Documentation for the filestorage """
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -10,6 +12,7 @@ from models.state import State
 
 
 class FileStorage:
+    """ The FileStorage class """
     __file_path = "file.json"
     __objects = {}
 
