00bdebfc708bf858c2c6cfdd3d436d934b41ad41
Eze Israel John <ezeisrael499@gmail.com>
2024-02-10 22:38:42 +0100

    Added some changes

--- models/base_model.py
+++ models/base_model.py
@@ -10,45 +10,14 @@ import models
 class BaseModel:
     """BaseModel that defines all common attributes/methods for other classes
     """
-
-    def __init__(self, *args, **kwargs):
-        """Method to assign attributes when a basemodel class is instantiated
-
-        Args:
-        id (str): a unique and randomly generated uuid for each instance
-            of Basemodel
-        created_at (datetime): Current date and time for a new instance
-              of Basemodel
-        updated_at (datetime): Current date and time when an instance of
-        Basemodel is created or updated
-        """
-        # self.id = str(uuid4())
-        # self.updated_at = datetime.now()
-        # self.created_at = datetime.now()
-        # models.storage.new(self)
-
-        if kwargs:
-            for key, value in kwargs.items():
-                # convert date time strings into datetime object
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                    # __class__ should not be added as an attribute
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-        def __str__(self):
-            """A method invoked when an instance of Basemodel class is printed
+    def __str__(self):
+        """A method invoked when an instance of Basemodel class is printed
 
             Returns:
             formatted string: returns class name, instance uuid and
             attributes
             """
-            return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates the public instance attribute updated_at with
@@ -70,3 +39,32 @@ class BaseModel:
         dictionary["updated_at"] = self.updated_at.isoformat()
         dictionary["__class__"] = self.__class__.__name__
         return dictionary
+
+    def __init__(self, *args, **kwargs):
+        """Method to assign attributes when a basemodel class is instantiated
+
+        Args:
+        id (str): a unique and randomly generated uuid for each instance
+            of Basemodel
+        created_at (datetime): Current date and time for a new instance
+              of Basemodel
+        updated_at (datetime): Current date and time when an instance of
+        Basemodel is created or updated
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                # convert date time strings into datetime object
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                    # __class__ should not be added as an attribute
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
