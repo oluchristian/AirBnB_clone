8bcb0d031349ec3c1d2961fcc7d1f50ac736b849
Eze Israel John <ezeisrael499@gmail.com>
2024-02-10 16:58:47 +0100

    Added test cases for the state module

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,3 +9,29 @@ from unittest import TestCase
 
 class TestState(TestCase):
     """ The Test case for """
+
+    def setUp(self) -> None:
+        """ The setup method"""
+        self.test_1 = State()
+
+    def tearDown(self) -> None:
+        """ The teardown method """
+        pass
+
+    def test_unrecognized_attribute(self):
+        """ The test unrecognized attribute """
+        with self.assertRaises(AttributeError):
+            self.test_1.no_attribute
+
+    def test_unrecognized_method(self):
+        """ The test unrecognized method"""
+        with self.assertRaises(AttributeError):
+            self.test_1.no_method
+
+    def test_class_attributes(self):
+        """ Test class attributes"""
+        self.assertEqual(self.test_1.name, "")
+
+    def test_instance_of_test_1(self):
+        """ Test the class instance """
+        self.assertIsInstance(self.test_1, State)
