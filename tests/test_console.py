288ef32b40a367e3c636bc0aca4139c697dda97f
Eze Israel John <ezeisrael499@gmail.com>
2024-02-11 23:47:33 +0100

    Changes made git push

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,6 +10,8 @@ from models.state import State
 from models.review import Review
 from datetime import datetime
 from unittest import TestCase
+from console import HBNBCommand
+from cmd import Cmd
 
 
 class TestConsole(TestCase):
@@ -19,4 +21,19 @@ class TestConsole(TestCase):
         pass
     
     def tearDown(self) -> None:
+        """ The tearDown Method"""
         pass
+
+    def test_the_prompt(self):
+        """ Checks if the prompt is hbnb """
+        self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
+
+    def test_the_instance_of_prompt_variable(self):
+        self.assertIsInstance(HBNBCommand.prompt, str)
+
+    def test_if_match_is_in_dict_of_HBNB(self):
+        self.assertIn("default", HBNBCommand.__dict__)
+
+    def test_registered_models_not_in_dict(self):
+        self.assertNotIn("__registered_models", HBNBCommand.__dict__)
+        self.assertNotIn("registered_models", HBNBCommand.__dict__)
